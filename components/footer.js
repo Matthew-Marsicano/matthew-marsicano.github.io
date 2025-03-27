e2038723f0d2abedc0ece5ee70e45f1d302a815c
Matthew-Marsicano <105601857+Matthew-Marsicano@users.noreply.github.com>
2025-03-27 03:27:27 -0400

    Package nav bar and footer into components

---
+++ components/footer.js
@@ -0,0 +1,26 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+       <footer>
+        Let's talk! Reach out on:
+        <div class="social-links">
+            <a href="https://www.linkedin.com/in/matthewmarsicano" target="_blank">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/Matthew-Marsicano" target="_blank">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="mailto:portfolio@matthewmarsicano.com">
+                <i class="fas fa-envelope"></i>
+            </a>
+        </div>
+    </footer>
+    `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
---
+++ components/nav.js
@@ -0,0 +1,33 @@
+class NavBar extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <nav>
+        <a href="/">Home</a>
+        <a href="/resume/">Resume</a>
+        <div class="dropdown">
+            <a href="/experience/" class="dropbtn">Experience</a>
+            <ul class="dropdown-content">
+                <li><a href="/experience/industry/">Industry</a></li>
+                <li><a href="/experience/research/">Research</a></li>
+            </ul>
+        </div>
+        <div class="dropdown">
+            <a href="/projects/" class="dropbtn">Projects</a>
+            <ul class="dropdown-content">
+                <li><a href="/projects/hardware/">Hardware</a></li>
+                <li><a href="/projects/software/">Software</a></li>
+                <li><a href="/projects/classwork/">Classwork</a></li>
+            </ul>
+        </div>
+        <a href="/gallery/">Certifications</a>
+        <a href="/gallery/">Gallery</a>
+        </nav> 
+    `;
+    }
+  }
+  
+  customElements.define('nav-component', NavBar);
